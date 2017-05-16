da93b0d8ded18366d83ee70953a94ad72ac4ed31
Kyle Hilton <kylehilton92@gmail.com>
2017-05-16 13:09:25 -0500

    cleans up code

--- sheets/ss.js
+++ sheets/ss.js
@@ -10,88 +10,85 @@ var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'sheets-credentials.json';
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(cb, authCb) {
-
-
-  var clientId = process.env.OAUTH_CLIENT_SECRET || "980683034451-l7k9t1h6sr82v4il32rruks7sh5gmkvj.apps.googleusercontent.com";
-  var clientSecret = process.env.OAUTH_CLIENT_SECRET || "GQeQd6eoAEjD1RAMi-rMJXuh";
-  var redirectUrl = process.env.SHEET_REDIRECT_URL || 'http://localhost:3000/redirectOAuth';
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, authCb);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      cb(oauth2Client);
-    }
-  });
-};
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken (oauth2Client, cb) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-
-  cb(authUrl);
-};
-
-function recieveToken (code) {
-
-  var clientId = process.env.OAUTH_CLIENT_SECRET || "980683034451-l7k9t1h6sr82v4il32rruks7sh5gmkvj.apps.googleusercontent.com";
-  var clientSecret = process.env.OAUTH_CLIENT_SECRET || "GQeQd6eoAEjD1RAMi-rMJXuh";
-  var redirectUrl = process.env.SHEET_REDIRECT_URL || 'http://localhost:3000/redirectOAuth';
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  oauth2Client.getToken(code, function (err, token) {
-    if (err) {
-      console.log('Error while trying to retrieve access token', err);
-      return;
-    }
-    oauth2Client.credentials = token;
-    storeToken(token);
-  });
-}
-
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken (token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
-}
-
 module.exports = {
-  auth : authorize,
-  recieveToken: recieveToken
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   *
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  auth: function (cb, authCb) {
+
+    var oauth2Client = getClient();
+
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+      if (err) {
+        getNewToken(oauth2Client, authCb);
+      } else {
+        oauth2Client.credentials = JSON.parse(token);
+        cb(oauth2Client);
+      }
+    });
+  },
+
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   *
+   * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback to call with the authorized
+   *     client.
+   */
+  getNewToken: function (oauth2Client, cb) {
+    var authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    });
+
+    cb(authUrl);
+  },
+
+  recieveToken: function (code) {
+    var utils = this;
+
+    var oauth2Client = getClient();
+
+    oauth2Client.getToken(code, function (err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      utils.storeToken(token);
+    });
+  },
+
+
+  /**
+   * Store token to disk be used in later program executions.
+   *
+   * @param {Object} token The token to store to disk.
+   */
+  storeToken: function (token) {
+    try {
+      fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+      if (err.code != 'EEXIST') {
+        throw err;
+      }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+  }
 };
+
+function getClient () {
+  var clientId = process.env.OAUTH_CLIENT_SECRET || "980683034451-l7k9t1h6sr82v4il32rruks7sh5gmkvj.apps.googleusercontent.com";
+  var clientSecret = process.env.OAUTH_CLIENT_SECRET || "GQeQd6eoAEjD1RAMi-rMJXuh";
+  var redirectUrl = process.env.SHEET_REDIRECT_URL || 'http://localhost:3000/redirectOAuth';
+  var auth = new googleAuth();
+  return new auth.OAuth2(clientId, clientSecret, redirectUrl);
+}
--- sheets/utils.js
+++ sheets/utils.js
@@ -7,91 +7,86 @@ var sheets = google.sheets('v4');
 
 
 // Creates new sheet, accepts a title for the sheet and a callback taking params of err and response
-var createSheet = function (title, cb, authCb) {
-	ss.auth(function (auth) {
-			sheets.spreadsheets.create({
-			resource: {
-				properties: {
-					title: title
-				}
-			},
-			auth: auth
-			}, cb);
-  	}, authCb);
-};
-
-function storeToken (code, cb) {
-	ss.recieveToken(code, cb);
-}
-
-// only workswhen there are less actives
-function exportRc(stakeId, cb) {
-	dbUtils.getStakeRcs(stakeId, function (error, response) {
-		// If there is a spreadsheet made for stake, it updates sheet and
-		// sends url. if there is no sheet, it creates sheet, updates it and sends it.
-		if (!response[0].sheetId) {
-			createSheet(response[0].stakeName, function (err,res) {
-				dbUtils.updateSheetId(stakeId, res.spreadsheetId)
-				updateSheet(res.spreadsheetId, 'A1', createValMatrix(response), cb);
-				cb(res.spreadsheetUrl);
-			}, cb)
-		} else {
-			updateSheet(response[0].sheetId, 'A1', createValMatrix(response), cb);
-  		cb('https://docs.google.com/spreadsheets/d/' + response[0].sheetId + '/edit');
-		}
-	})
-};
-
-function updateSheet (id, range, vals, cb) {
-	ss.auth(function (auth) {
-		sheets.spreadsheets.values.append({
-			spreadsheetId:id,
-			range: range,
-			valueInputOption: 'RAW',
-			resource: {
-				values: vals
-			},
-			auth: auth
-		}, cb);
-	})
-};
-
-
-function readSheet (id, range, cb) {
-	ss.auth(function (auth) {
-		sheets.spreadsheets.values.get({
-			spreadsheetId: id,
-			range: range,
-			auth: auth
-		}, cb);
-	})
-}
-
-function getSheets (id, cb, authCb) {
-	ss.auth(function (auth) {
-	  sheets.spreadsheets.get({
-  		spreadsheetId: id,
-  		ranges: [],
-	    includeGridData: false,
-	    auth: auth
-		}, cb)
-	}, authCb);
-}
-
-function createValMatrix (rcs) {
-	var matrix = [['Current Unit', 'Name','Bishop/Branch President','Age','Gender','Date Confirmed','Status','Attended Church this month','Responsibility/Calling','Home Teacher','Visiting Teachers','Temple']];
-
-	for (var i = 0; i < rcs.length; i++) {
-		var rc = rcs[i];
-		matrix.push([rc.unit, rc.name,, rc.age, rc.gender, rc.bd,,, rc.hters, rc.vters,]);
-	};
-	return matrix;
-};
-
 module.exports = {
-	exportRc: exportRc,
-	storeToken: storeToken,
-	readSheet: readSheet,
-	getSheets: getSheets
-};
+	createSheet: function (title, cb, authCb) {
+		ss.auth(function (auth) {
+				sheets.spreadsheets.create({
+				resource: {
+					properties: {
+						title: title
+					}
+				},
+				auth: auth
+				}, cb);
+	  	}, authCb);
+	},
 
+	storeToken: function (code, cb) {
+		ss.recieveToken(code, cb);
+	},
+
+	// only workswhen there are less actives
+	exportRc: function(stakeId, cb) {
+		var utils = this;
+		dbUtils.getStakeRcs(stakeId, function (error, response) {
+			// If there is a spreadsheet made for stake, it updates sheet and
+			// sends url. if there is no sheet, it creates sheet, updates it and sends it.
+			if (!response[0].sheetId) {
+				utils.createSheet(response[0].stakeName, function (err,res) {
+					dbUtils.updateSheetId(stakeId, res.spreadsheetId)
+					utils.updateSheet(res.spreadsheetId, 'A1', utils.formatVals(response), cb);
+					cb(res.spreadsheetUrl);
+				}, cb)
+			} else {
+				utils.updateSheet(response[0].sheetId, 'A1', utils.formatVals(response), cb);
+	  		cb('https://docs.google.com/spreadsheets/d/' + response[0].sheetId + '/edit');
+			}
+		})
+	},
+
+	updateSheet: function (id, range, vals, cb) {
+		ss.auth(function (auth) {
+			sheets.spreadsheets.values.append({
+				spreadsheetId:id,
+				range: range,
+				valueInputOption: 'RAW',
+				resource: {
+					values: vals
+				},
+				auth: auth
+			}, cb);
+		})
+	},
+
+	readSheet: function (id, range, cb) {
+		ss.auth(function (auth) {
+			sheets.spreadsheets.values.get({
+				spreadsheetId: id,
+				range: range,
+				auth: auth
+			}, cb);
+		})
+	},
+
+	getSheets: function (id, cb, authCb) {
+		ss.auth(function (auth) {
+		  sheets.spreadsheets.get({
+	  		spreadsheetId: id,
+	  		ranges: [],
+		    includeGridData: false,
+		    auth: auth
+			}, cb)
+		}, authCb);
+	},
+
+	formatVals: function (rcs) {
+		var matrix = [['Current Unit', 'Name','Bishop/Branch President','Age','Gender','Date Confirmed','Status','Attended Church this month','Responsibility/Calling','Home Teacher','Visiting Teachers','Temple']];
+
+		for (var i = 0; i < rcs.length; i++) {
+			var rc = rcs[i];
+			matrix.push([rc.unit, rc.name,, rc.age, rc.gender, rc.bd,,, rc.hters, rc.vters,]);
+		};
+		return matrix;
+	}
+
+};
